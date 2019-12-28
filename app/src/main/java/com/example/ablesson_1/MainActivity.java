89a9753b40a8dff52579c1c2e17eb07b75273d69
RaFFiaNBui <Raffian_bui@mail.ru>
2019-12-28 17:52:28 +0300

    Выполнено ДЗ к уроку №2

--- app/src/main/java/com/example/ablesson_1/MainActivity.java
+++ app/src/main/java/com/example/ablesson_1/MainActivity.java
@@ -3,12 +3,38 @@ package com.example.ablesson_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final View.OnClickListener onClickListener_loading = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            frameLayout_first_page.setVisibility(View.VISIBLE);
+            linearLayout_loading.setVisibility(View.GONE);
+        }
+    };
+
+
+    private LinearLayout linearLayout_loading;
+    private FrameLayout frameLayout_first_page;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_city);
+
+        /*frameLayout_first_page = findViewById(R.id.FrameLayout_first_page);
+        linearLayout_loading = findViewById(R.id.LinearLayout_loading);
+
+        Button button_loading = findViewById(R.id.button_loading);
+        button_loading.setOnClickListener(onClickListener_loading);*/
+
+
     }
 }
