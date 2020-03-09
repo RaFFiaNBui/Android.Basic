0e1b0576e9b526782000c49221cf3693260ca861
RaFFiaNBui <Raffian_bui@mail.ru>
2020-03-09 15:50:25 +0300

    элементы погоды с сервера

---
+++ DRAFT/app/src/main/java/com/example/ablesson_1/model/Wind.java
@@ -0,0 +1,13 @@
+package com.example.ablesson_1.model;
+
+public class Wind {
+    private int speed;
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+}
